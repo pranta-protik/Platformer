449a74636ba3499ae5764d944005aa9d6462ec10
Pranta Protik <pranta.cseku@gmail.com>
2023-08-07 01:00:01 +0600

    Additional packages imported and installed

---
+++ Assets/MyTools/Setup.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using UnityEngine;
+using static System.IO.Path;
+using static System.IO.Directory;
+using static UnityEditor.AssetDatabase;
+
+public static class Setup
+{
+    [MenuItem("Tools/Setup/Create Default Folders")]
+    public static void CreateDefaultFolders()
+    {
+        Folders.CreateDefault("_Projects", "Animation", "Art", "Materials", "Prefabs", "ScriptableObjects", "Scripts", "Settings");
+        Refresh();
+    }
+
+    private static class Folders
+    {
+        public static void CreateDefault(string root, params string[] folders)
+        {
+            var fullPath = Combine(Application.dataPath, root);
+            foreach (var folder in folders)
+            {
+                var path = Combine(fullPath, folder);
+                if (!Exists(path))
+                {
+                    CreateDirectory(path);
+                }
+            }
+        }
+    }
+}
