330968d6244441b1db881b9c0fa8afa754929e41
Pranta Protik <pranta.cseku@gmail.com>
2023-08-07 11:09:52 +0600

    Inspector lock shortcut added

---
+++ Assets/MyTools/LockInspector.cs
@@ -0,0 +1,34 @@
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class LockInspector
+{
+    [MenuItem("Edit/Lock Inspector %l")]
+    public static void Lock()
+    {
+        ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+
+        foreach (var activeEditor in ActiveEditorTracker.sharedTracker.activeEditors)
+        {
+            if (activeEditor.target is not Transform) continue;
+
+            var transform = (Transform)activeEditor.target;
+
+            var propInfo = transform.GetType().GetProperty("constrainProportionsScale", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            if (propInfo == null) continue;
+
+            var value = (bool)propInfo.GetValue(transform, null);
+            propInfo.SetValue(transform, !value, null);
+        }
+        
+        ActiveEditorTracker.sharedTracker.ForceRebuild();
+    }
+
+    [MenuItem("Edit/Lock Inspector %l", true)]
+    public static bool Valid()
+    {
+        return ActiveEditorTracker.sharedTracker.activeEditors.Length != 0;
+    }
+}
